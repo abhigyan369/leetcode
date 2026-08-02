c4bd90685c773e0d92fbe17033f8426797d23ba4
Abhigyan Kumar <155965263+abhigyan369@users.noreply.github.com>
2026-08-02 14:50:40 +0530

    Time: 67 ms (5.78%), Space: 22.5 MB (5.25%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -0,0 +1,26 @@
+
+class Solution:
+    
+    t = [[-1]*1001 for _ in range(200)]
+    def solve(self,idx,nums,target):
+        n = len(nums)
+        if target == 0:
+            return 1
+        if idx >= n or target < 0:
+            return 0
+        if Solution.t[idx][target] != -1:
+            return Solution.t[idx][target]
+        
+        take_idx = self.solve(0,nums,target-nums[idx])
+        reject_idx = self.solve(idx+1,nums,target)
+
+        Solution.t[idx][target] = take_idx + reject_idx
+        return Solution.t[idx][target]
+        
+
+
+
+
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        Solution.t = [[-1]*1001 for _ in range(200)]
+        return self.solve(0,nums,target)
