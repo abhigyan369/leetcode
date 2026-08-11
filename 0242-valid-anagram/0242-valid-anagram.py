1ce7cad94c33455febdfa097179c1fa42475caf9
Abhigyan Kumar <155965263+abhigyan369@users.noreply.github.com>
2026-08-12 00:03:22 +0530

    Time: 11 ms (77.42%), Space: 19.4 MB (75.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        ## anagram-
+        ## "anagram" - a -> 3, n->1, g->1, r->1, m->1
+        ## nagaram = a -> 3, n->1, g->1, r->1, m->1
+
+        mp1 = {}
+        for char in s:
+            mp1[char] = mp1.get(char,0) + 1
+        mp2 = {}
+        for char in t:
+            mp2[char] = mp2.get(char,0) + 1
+        return mp1 == mp2
