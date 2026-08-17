c2b895206f970c94fe46a09f6db676e4236e95e1
Abhigyan Kumar <155965263+abhigyan369@users.noreply.github.com>
2026-08-17 12:07:01 +0530

    Time: 115 ms (44.87%), Space: 38.1 MB (32.48%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -1,19 +1,37 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        s = set()
         temp1 = headA
         temp2 = headB
+        cnt1, cnt2 = 0, 0
+
+        # Count nodes in headA
         while temp1 is not None:
-            if temp1 not in s:
-                s.add(temp1)
+            cnt1 += 1
             temp1 = temp1.next
+
+        # Count nodes in headB
         while temp2 is not None:
-            if temp2 in s:
-                return temp2
+            cnt2 += 1
             temp2 = temp2.next
+
+        # Start pointers at the heads
+        c1 = headA
+        c2 = headB
+
+        # Move the pointer of the longer list ahead
+        if cnt1 > cnt2:
+            for _ in range(cnt1 - cnt2):
+                c1 = c1.next
+        else:
+            for _ in range(cnt2 - cnt1):
+                c2 = c2.next
+
+        # Move both pointers together
+        while c1 is not None and c2 is not None:
+            if c1 == c2:
+                return c1
+
+            c1 = c1.next
+            c2 = c2.next
+
+        return None
