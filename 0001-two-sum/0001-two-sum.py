c5ad9c62a9e5d3be702d49c334dfff6e0d12c9f2
Abhigyan Kumar <155965263+abhigyan369@users.noreply.github.com>
2026-08-24 21:08:44 +0530

    Time: 4141 ms (5%), Space: 19.7 MB (81.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,15 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
-        mp = {}
         for i in range(n):
-            remaining = target - nums[i]
-            if remaining in mp:
-                return [mp[remaining], i]
+            for j in range(n):
+                if i != j:
+                    if nums[i] + nums[j] == target:
+                        return [i,j]
+        return [-1,-1]
 
-            mp[nums[i]] = i
-        return []       
-
-### nums = [2,7,11,15], target = 9
-## mp = {2:0, 7:1, 11:2, 15:3}
-## rem = 9-2 => 7 
